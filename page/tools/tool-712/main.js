7e921887c3b52299f8fba5588830f6d00d0a20f2
橙续缘 <lixiaohui2019@sina.cn>
2019-11-08 13:44:39 +0800

    新增工具：功率换算[712]

---
+++ page/tools/tool-712/main.js
@@ -0,0 +1,67 @@
+const data = [
+  {
+    "id": "ps",
+    "title": "米制马力",
+    "value": "",
+    "unit": 1
+  },
+  {
+    "id": "kcal/s",
+    "title": "千卡/秒",
+    "value": "",
+    "unit": 0.1757842
+  },
+  {
+    "id": "kW",
+    "title": "千瓦",
+    "value": "",
+    "unit": 0.7354987
+  },
+  {
+    "id": "hp",
+    "title": "英制马力",
+    "value": "",
+    "unit": 0.9863201
+  },
+  {
+    "id": "ft·lb/s",
+    "title": "英尺·磅/秒",
+    "value": "",
+    "unit": 542.4760385
+  },
+  {
+    "id": "J/s",
+    "title": "焦耳/秒",
+    "value": "",
+    "unit": 735.49875
+  },
+  {
+    "id": "Btu/s",
+    "title": "英热单位/秒",
+    "value": "",
+    "unit": 0.6971183
+  },
+  {
+    "id": "kg·m/s",
+    "title": "公斤·米/秒/秒",
+    "value": "",
+    "unit": 75
+  },
+  {
+    "id": "N·m/s",
+    "title": "牛顿·米/秒",
+    "value": "",
+    "unit": 735.49875
+  }
+]
+
+Page.createTool({
+
+  toolId: 712,
+  toolName: '功率换算',
+  toolCate: [103],
+
+  data: {
+    data
+  }
+})
