3e1c058f6872a8e2e0e115e6f2449ef7e1f9f674
橙续缘 <lixiaohui2019@sina.cn>
2019-11-09 12:53:32 +0800

    新增工具：表情包生成器之谁赞成谁反对[769]

---
+++ page/tools/tool-769/main.js
@@ -0,0 +1,14 @@
+Page.createTool({
+  toolId: 769,
+  toolName: '表情包生成器之谁赞成谁反对',
+  toolCate: [105],
+
+  data: {
+    placeholders: [
+      '我话说完了',
+      '谁赞成',
+      '谁反对',
+      '我反对'
+    ]
+  }
+})
