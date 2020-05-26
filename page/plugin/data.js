65794392086958a58edab7fb01a074fd9efb5ed4
橙续缘 <lixiaohui2019@sina.cn>
2020-05-26 20:53:36 +0800

    优化：控制插件是否启用

--- page/plugin/data.js
+++ page/plugin/data.js
@@ -6,37 +6,42 @@ const plugins = [
     name: '纯音乐频道',
     path: '/page/plugins/plugin-100/main',
     image: 'https://s1.ax1x.com/2020/05/25/t99tBT.png',
-    hidden: false
+    hidden: false,
+    enable: false,
   },
   {
     id: 101,
     name: '漫画屋',
     path: '/page/plugins/plugin-101/main',
     image: 'https://s1.ax1x.com/2020/05/25/t998cq.png',
-    hidden: false
+    hidden: false,
+    enable: true,
   },
   {
     id: 102,
     name: '许愿树',
     path: '/page/plugins/plugin-102/home/main',
     image: 'https://s1.ax1x.com/2020/05/25/t99YuV.png',
-    hidden: false
+    hidden: false,
+    enable: false,
   },
   {
     id: 103,
     name: '管理后台',
     path: '/page/plugins/plugin-103/page/home/main',
     image: 'https://s1.ax1x.com/2020/05/25/t9933n.png',
-    hidden: true
+    hidden: true,
+    enable: true,
   },
   {
     id: 104,
     name: '食谱猫',
     path: '/page/plugins/plugin-104/page/menu/main',
     image: 'https://s1.ax1x.com/2020/05/25/t99Gj0.png',
-    hidden: false
+    hidden: false,
+    enable: true,
   },
-];
+].filter(item => item.enable);
 
 export default createStorage('plugins_manager', {
 
